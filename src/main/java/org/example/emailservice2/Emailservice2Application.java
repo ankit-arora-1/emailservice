3a38bf06ccf2a554568c72ce616d8939a3fd9dec
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-03-14 08:29:17 +0530

    Created email service

---
+++ src/main/java/org/example/emailservice2/Emailservice2Application.java
@@ -0,0 +1,13 @@
+package org.example.emailservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emailservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emailservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailservice2/Emailservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emailservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
